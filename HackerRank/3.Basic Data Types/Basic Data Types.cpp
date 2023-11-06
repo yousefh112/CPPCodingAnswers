9026854c0644c9e95e9818b72e39a833c24464c4
yousefh112 <yousef_hesham.yh112@icloud.com>
2023-11-06 19:40:53 +0200

    adding HackerRank Third Problem

---
+++ HackerRank/3.Basic Data Types/Basic Data Types.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a;
+  long b;
+  char c;
+  float d;
+  double e;
+  cin >> a >> b >> c >> d >> e;
+  cout << a << endl; 
+  cout << b << endl;
+  cout << c << endl;
+  cout << fixed << d << endl;
+  cout << fixed << e << endl;
+    return 0;
+}
