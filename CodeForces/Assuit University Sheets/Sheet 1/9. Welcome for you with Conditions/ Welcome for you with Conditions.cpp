352f80a8cb1088a8becb42793188a8f72a0d76d6
yousefh112 <yousef_hesham.yh112@icloud.com>
2023-12-06 04:58:39 +0200

    Adding NInth Problem in Sheet 1

--- CodeForces/Assuit University Sheets/Sheet 1/9. Welcome for you with Conditions/ Welcome for you with Conditions.cpp
+++ CodeForces/Assuit University Sheets/Sheet 1/9. Welcome for you with Conditions/ Welcome for you with Conditions.cpp
@@ -2,15 +2,19 @@
 using namespace std;
 int main()
 {
-    int A, B;
+    int A, B; // Declare two integer variables A and B to store user input.
+
+    // Prompt the user to enter values for A and B.
     cin >> A >> B;
+    // Check if A is greater than or equal to B.
     if (A > B || A == B)
     {
-        cout << "Yes" << endl;
+        cout << "Yes" << endl; // If the condition is true, print "Yes" to the console.
     }
     else
     {
-        cout << "No" << endl;
+        cout << "No" << endl; // If the condition is false, print "No" to the console.
     }
+
     return 0;
 }
