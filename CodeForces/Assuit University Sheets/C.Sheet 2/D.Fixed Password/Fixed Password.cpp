910e9d1cf3e22258d5fb8fcd7ce10fa9b50229df
yousefh112 <yousef_hesham.yh112@icloud.com>
2023-12-28 11:00:02 +0200

    Adding Fixed Password Problem in Sheet 2

--- CodeForces/Assuit University Sheets/C.Sheet 2/D.Fixed Password/Fixed Password.cpp
+++ CodeForces/Assuit University Sheets/C.Sheet 2/D.Fixed Password/Fixed Password.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
 int main()
 {
+    // variable to store user input
     int N;
-    vector<int> v;
-    while (cin >> N)
+
+    // while loop to take inputs until 1999 is encountered
+    while (true)
     {
-        v.push_back(N);
+
+        // check if the current input is 1999
         if (N == 1999)
         {
+            // print "Correct" and exit the loop
             cout << "Correct" << endl;
             break;
         }
         else
         {
+            // print "Wrong" for inputs that are not 1999
             cout << "Wrong" << endl;
         }
     }
